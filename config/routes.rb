a9d9f0e0c365bfe17449de0f3893fe79facd2c50
stephanie yawa de wolfe <stephdewolfe@gmail.com>
2017-11-21 10:16:56 -0600

    add scope products, brands, and categories to admin namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+
+scope :admin do
   resources :products
   resources :brands
   resources :categories
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  end
 end
+# changes the path but not the link.
